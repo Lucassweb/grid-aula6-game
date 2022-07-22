337abc4e932baec375cf6509e25a5f618c762fb8
Lucas <lucas.26.04@hotmail.com>
2022-07-22 18:21:33 -0300

    projeto finalizado

---
+++ js/index.js
@@ -0,0 +1,22 @@
+const data = [
+    {
+      image: "./img/1011170.png",
+      title: "Kanao Tsuyuri",
+      description: "Kanao Tsuyuri é a Tsuguko, a futura Hashira da Respiração das Flores. Ela teve um passado bem abusivo e por isso reprime ao máximo suas emoções. Também não confia em suas próprias escolhas e usa uma moeda quando precisa tomar uma decisão. Acaba se aproximando de Tanjiro com o passar do tempo."
+    },
+
+    {
+        image: "./img/1011170.png",
+        title: "Kanao Tsuyuri",
+        description: "Kanao Tsuyuri é a Tsuguko, a futura Hashira da Respiração das Flores. Ela teve um passado bem abusivo e por isso reprime ao máximo suas emoções. Também não confia em suas próprias escolhas e usa uma moeda quando precisa tomar uma decisão. Acaba se aproximando de Tanjiro com o passar do tempo."
+    },
+
+    {
+        image: "./img/1011170.png",
+        title: "Kanao Tsuyuri",
+        description: "Kanao Tsuyuri é a Tsuguko, a futura Hashira da Respiração das Flores. Ela teve um passado bem abusivo e por isso reprime ao máximo suas emoções. Também não confia em suas próprias escolhas e usa uma moeda quando precisa tomar uma decisão. Acaba se aproximando de Tanjiro com o passar do tempo."
+    },
+
+
+    
+]
